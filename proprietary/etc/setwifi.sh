d5d21f40c5ce06570e9b9a182ae92c46746f3cdf
thekraven <savethestyle1@gmail.com>
2012-02-19 15:54:34 -0600

    set wifi to eth0
    Change-Id: I6f0d05d740ceff77f77f139d2fac803d0e75fb2d

--- proprietary/etc/setwifi.sh
+++ proprietary/etc/setwifi.sh
@@ -1,5 +1,5 @@
 sleep 10
-/system/xbin/iwconfig wlan0 power all
+/system/xbin/iwconfig eth0 power all
 sleep 1
-/system/xbin/iwconfig wlan0 txpower 30mW
+/system/xbin/iwconfig eth0 txpower 30mW
 log -p i -t iwconfig "WIFI: power management enabled"
